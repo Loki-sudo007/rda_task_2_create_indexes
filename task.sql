d53dd0c95a6f780595a141e7f5b17080aa75eedb
Vitalii <stalchenkovitaliy@gmail.com>
2026-07-08 20:02:01 +0400

    Add index Date

--- task.sql
+++ task.sql
@@ -2,6 +2,7 @@ USE ShopDB;
 
 CREATE INDEX Email ON Customers (Email);
 CREATE INDEX Name ON Products (Name);
+CREATE INDEX Date ON  Orders (Date);
 CREATE INDEX CustomerID ON Orders (CustomerID);
 CREATE INDEX OrderID ON OrderItems (OrderID);
 CREATE INDEX ProductID ON OrderItems (ProductID);
